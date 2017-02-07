0572c09817404bf70131c3c0c7630444764d9142
Oboo Chin <cheng.12@live.cn>
2017-02-07 13:25:17 +0800

    add info.

--- refactor/chart.js
+++ refactor/chart.js
@@ -4,8 +4,28 @@ import {
 	Text
 } from 'react-native'
 
+import {
+	Bar,
+	Doughnut,
+	Histogram,
+	Line,
+	Pie,
+	Radar
+}  form './charts'
+
 export default class RNAChart extends Component{
 	render(){
-		return <View/>
+		return this.getChart()
+	}
+
+	getChart = () => {
+		switch( this.props.type ){
+			case "line":return <Line ...this.props/>;
+			case "bar":return <Bar ...this.props/>;
+			case "pie":return <Pie ...this.props/>;
+			case "doughnut":return <Doughnut ...this.props/>;
+			case "histogram":return <Histogram ...this.props/>;
+			case "radar":return <Radar ...this.props/>;
+		}
 	}
 }
---
+++ refactor/charts/index.js
@@ -0,0 +1,17 @@
+import * as {
+	Bar,
+	Doughnut,
+	Histogram,
+	Line,
+	Pie,
+	Radar
+} from './'
+
+export default {
+	Bar,
+	Doughnut,
+	Histogram,
+	Line,
+	Pie,
+	Radar
+} 
