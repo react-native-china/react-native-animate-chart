4c41987d0112211c58b632c40e1c08e11fa9bc01
Oboo Chin <cheng.12@live.cn>
2017-02-08 08:32:51 +0800

    move test script to test folder.

--- __test__/implements.test.js
+++ __test__/implements.test.js
@@ -1,4 +1,4 @@
-const { roundingRange } = require('./implements');
+const { roundingRange } = require('../implements');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(roundingRange([100,2001])).toBe(2010);
